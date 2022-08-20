5681f02512cedf3da7aa87b160ffb98085ef4389
Enrique Isidro Sánchez Olvera <0208738@up.edu.mx>
2022-08-19 20:01:14 -0500

    jhgkj

---
+++ script.js
@@ -0,0 +1,21 @@
+//Legado
+var var1='var1'
+
+const cosnst1='const1'
+let let1='let1'
+let1=14
+
+console.log('let1', let1)
+console.log(let1)
+console.warn('const1', 'let1', const1, let1)
+
+// Legado
+function name(params){
+
+}
+
+const myFunction=(txt)=>{
+    console.log(txt, 'from func')
+}
+
+myFunction('Hola')
